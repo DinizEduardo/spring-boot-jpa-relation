8c34774f5b9bc05a0d563997ebb9957bd495f0a8
DinizEduardo <eduardo.diniz987@gmail.com>
2022-11-28 23:16:28 -0300

    feat: initial commit

---
+++ src/test/java/br/com/spring/jpa/empresas/EmpresasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.jpa.empresas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
